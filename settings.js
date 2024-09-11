190c4c4749447849430c9796bb6ed64199fb8cd8
ag8o7 <server64462@gmail.com>
2024-09-12 03:25:09 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6281584729418']
+global.botname = 'Rxhl Bot'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Rxhl Official"
+global.sticker2 = "🌜"
